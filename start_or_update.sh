c1aee48deb2069d882108fd7e4758ec08d8f4e55
yohan <783b8c87@scimetis.net>
2019-12-04 11:09:25 +0100

    Using HTTPS instead of SSH to query Git repo so the SSH key is not needed.

--- start_or_update.sh
+++ start_or_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unset VERSION_APACHE_FPM
 unset VERSION_PHP_FPM
-VERSION_APACHE_FPM=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-apache-for-fpm.git| head -1 | cut -f 1|cut -c -10) \
-VERSION_PHP_FPM=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-php5-fpm.git| head -1 | cut -f 1|cut -c -10) \
+VERSION_APACHE_FPM=$(git ls-remote https://git.scimetis.net/yohan/docker-apache-for-fpm.git| head -1 | cut -f 1|cut -c -10) \
+VERSION_PHP_FPM=$(git ls-remote https://git.scimetis.net/yohan/docker-php5-fpm.git| head -1 | cut -f 1|cut -c -10) \
  sudo -E bash -c 'docker-compose up -d'
